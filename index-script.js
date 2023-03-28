dacfae6ff7fb2752ba0eeb3748c14a37609cf4e7
Luis Vieira <luisernani87@gmail.com>
2023-03-28 20:03:43 -0300

    finishing 2023-03-28, navbar email popup button working ok

--- index-script.js
+++ index-script.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', () =>{
+
+    // NAVBAR FADE OUT EFFECT 
     window.addEventListener('scroll', () => {
         let navbar = document.querySelector('.navbar');
         let scrollPosition = window.scrollY;
@@ -10,5 +12,17 @@ window.addEventListener('DOMContentLoaded', () =>{
     });
 
 
+    // email POPUP
+    let emailPopup = document.querySelector('.email-popup');
+    let btn = document.querySelector('.email-btn');
+    let btnClose = document.querySelector('.email-popup-close-btn');
+
+    btn.onclick = function(){
+        emailPopup.style.display = 'block';
+    }
+    btnClose.onclick = function(){
+        emailPopup.style.display = 'none';
+        
+    }
 
 });
